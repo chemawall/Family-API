c9014ccf4740670e9ab092e54aecd7b6865098ea
chemawall <jmmuro88@gmail.com>
2023-01-19 19:40:37 +0000

    avances en delete_member

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,19 +13,30 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+            "id":4,
+            "first_name":"Jose",
+            "last_name": "MUro"},
+            {"id":2,
+            "first_name":"Joaqui",
+            "last_name":"Vazquez"
+            }]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member["id"] = self._generateId()
+        # fill this method and update the return        
+        self._members.append(member)
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for element in family.get_all_members():
+            for e in element.values():
+                if e == (id):
+                    family.get_all_members().remove(element)
 
     def get_member(self, id):
         # fill this method and update the return
@@ -34,3 +45,20 @@ class FamilyStructure:
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+member = {
+    "id":[],
+    "first_name": "Peter",
+    "last_name": "Jackson",
+    "age": 44,
+    "lucky_numbers": [{1,2,3}]
+}
+
+
+family= FamilyStructure(last_name="Jackson")
+#print(family.last_name)
+print(family.get_all_members())
+family.delete_member(4)
+print(family.get_all_members())
+
+
